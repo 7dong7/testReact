4a6631266c59cc282d125e79767e2986ff570e0f
7dong7 <tmdkdl777@gmail.com>
2025-03-07 18:17:07 +0900

    react 프로젝트 생성

--- section05/src/main.jsx
+++ section05/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App /> {/* app 컴퍼넌트를 랜더링 하고 있다 import App from './App.jsx' 를 통해서 */}
+    <App />
   </StrictMode>,
 )
