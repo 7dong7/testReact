5a1c3611fdd3136b8df290e54ade3b315e504985
7dong7 <tmdkdl777@gmail.com>
2025-03-09 02:56:48 +0900

    이벤트 핸들링

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -7,8 +7,22 @@
  * @constructor
  */
 const Button = ({ children, text, color = "black"}) => {
+
+    // 이런 이벤트가 발생할때는 이벤트 객체를 같이 보낸다
+        // SyntheticBaseEvent: 합성 이벤트 객체를 말한다
+        // 합성 이벤트 객체는 모든 웹 브라우저의 이벤트 객체를 하나로 통일한 형태 (크롬, 사파리, 엣지, 웨일, 파폭 등)
+    const onClickButton = (e) => {
+        console.log(e);
+        console.log(text);
+    };
+
     return (
-        <button style={{color: color}}>
+        <button onClick={onClickButton} // 콜백함수를 전달하듯이 ()를 빼고 전달한다
+            // onMouseEnter={onClickButton}
+        // <button onClick={() => {
+        //     console.log(text);
+        // }} // 이벤트를 처리하는 함수를 이벤트 핸들러라고 한다
+            style={{color: color}}>
             {text} - {color.toUpperCase()}
             {children}
         </button>
